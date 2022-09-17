32c06f290684e4a96ed7944eeb37fa8451bdbccc
ME-CDE <godswillanwuli@gmail.com>
2022-09-17 16:44:08 +0100

    a

--- src/Component/ReadMore.js
+++ src/Component/ReadMore.js
@@ -56,7 +56,7 @@ const ReadMore = () => {
       {data2 &&
       <> 
       <div className="w-[86%] xs:w-10/12 mx-auto" onLoad={()=>setLoading(false)}>
-        <img src={data.coverImage} alt="coverImage" className="w-full rounded-md mt-20"/>
+        <img src={data.coverImage} alt="coverImage" className="w-full rounded-md mt-20 max-h-[90vh]"/>
         <p className="w-max px-3 text-[17px] text-[#FDFEFF] rounded-md mt-[43px]" style={style}>{data.categories}</p>
         <h1 className="font-bold text-[35px] xxxs:text-[40px] xxs:text-[47px] xs:text-[52px] sm:text-[56px] md:text-[58px] lg:text-[62px] 2xl:w-[1300px] max-w-[100%] leading-[1.1em] mt-[28px] mb-[22.5px] text-primaryBlack">{data.title}</h1>
         <div className="w-full border-b pb-[26.7px] border-[#DDDDDD] mb-14">
